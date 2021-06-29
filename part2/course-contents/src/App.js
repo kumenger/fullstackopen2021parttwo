4795074ba6aa2d7fe93dff7929afa4fa3b7d70c4
kumenger <44558777+kumenger@users.noreply.github.com>
2021-06-29 15:16:25 -0600

    2a done

--- part2/course-contents/src/App.js
+++ part2/course-contents/src/App.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import Course  from './components/Course '
 
+
 const App=()=>{
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name:"Redux-Form",
-        exercises:12,
-        id:13
-      },
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-    ]
-  }
-  return(
-    <Course course={course}/>
+  
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+ 
+  return(< > 
+     {course.map(x=><div key={x.id}><Course course={x} /></div>)}
+   </>
   )
 }
 
--- part2/course-contents/src/components/Course .js
+++ part2/course-contents/src/components/Course .js
@@ -1,20 +1,42 @@
-import react from 'react'
-const Header=(props)=>{
-    return <><h1>{props.header}</h1></>
-}
-const Content=(props)=>{
-    return<div>
-          {props.parts.map(x=><ul key={x.id}><li><Parts  part={x.name} excersice={x.exercises}/></li></ul>)}
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.header}</h1>
+    </>
+  );
+};
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((x) => (
+        <ul key={x.id}>
+          <li >
+            <Parts part={x.name} excersice={x.exercises} />
+          </li>
+        </ul>
+      ))}
     </div>
-}
-const Parts=(props)=>{
- return <>{props.part}   {props.excersice}</>
-}
-const Course =({course})=>{
-   
-    return (<div>
-         <Header header={course.name}/>
-         <Content parts={course.parts} />
-    </div>)
-}
-export default Course
+  );
+};
+const Parts = (props) => {
+  return (
+    <>
+      {props.part} {props.excersice}
+    </>
+  );
+};
+
+const Total = (props) => {
+  return <> Total of {props.total} exercises </>;
+};
+const Course = (props) => {
+  let allsums = props.course.parts.reduce((acc, curval) => acc + curval.exercises, 0);
+  return (
+    <div>
+      <Header header={props.course.name} />
+      <Content parts={props.course.parts}  />
+      <Total total={allsums} />
+    </div>
+  );
+};
+export default Course;
